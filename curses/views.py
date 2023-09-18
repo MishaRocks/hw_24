4c56506d660046eebf926a50a17f819a29e734d6
MishaRocks <tehobox@yandex.ru>
2023-09-18 21:00:55 +0300

    hw_25.1

--- curses/views.py
+++ curses/views.py
@@ -5,11 +5,13 @@ from rest_framework.viewsets import ModelViewSet
 
 from curses.models import Curs
 from curses.serializers import CursSerializer
+from permissions import ModeratorAndObjectOwner
 
 
 class CursViewSet(ModelViewSet):
     queryset = Curs.objects.all()
     serializer_class = CursSerializer
 
+    permission_classes = [ModeratorAndObjectOwner]
 
 
--- permissions.py
+++ permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsModerator(BasePermission):
 
     def has_permission(self, request, view):
-        if request.user.groups.filter(name='Moderators'):
+        if request.user.groups.filter(name='moderators'):
             return True
 
 
@@ -13,3 +13,12 @@ class IsOwner(BasePermission):
     def has_permission(self, request, view):
         if request.user == view.get_object().user:
             return True
+
+
+class ModeratorAndObjectOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.groups.filter(name='moderators').exists() and request.method in ['GET', 'PUT', 'PATCH']:
+            return True
+        if request.user == obj.creator:
+            return True
+        return False
