e11555bdedd0722b95fe566689d21488ea1c79d3
MishaRocks <tehobox@yandex.ru>
2023-09-21 21:01:40 +0300

    hw_25.2 - validator

--- lessons/serializers.py
+++ lessons/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from lessons.models import Lesson
+from lessons.validators import ValidatorLinks
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [ValidatorLinks(field='description')]
