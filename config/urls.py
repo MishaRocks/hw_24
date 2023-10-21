41924e99fb530737cb2cb083db4f4292aa83d61d
MishaRocks <tehobox@yandex.ru>
2023-10-21 14:25:02 +0300

    docker + drf_yasg

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +24,7 @@ urlpatterns = [
     path('payments/', include('payments.urls')),
     path('users/', include('users.urls')),
     path('subscribes/', include('subscribes.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
